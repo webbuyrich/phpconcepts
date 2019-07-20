0d7c54fa1fe95b6676ac8085fa543d49cca60451
webbuyrich <webbuyrich@gmail.com>
2019-07-20 01:15:11 -0700

    printed hello world

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>1.1 Hello World</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        
+    </head>
+    <body>
+    	<b><?php print "Hello World"; ?></b>
+    </body>    
+</html>
