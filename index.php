df22591cdc81056eba8587e61376aea84d725e0f
webbuyrich <webbuyrich@gmail.com>
2019-07-20 01:21:33 -0700

    Added form and php file to show submitted name

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
         
     </head>
     <body>
-    	<b><?php print "Hello World"; ?></b>
+    	<form method="POST" action="sayhello.php">
+    		Your Name:<input type="text" name="user"/>
+    		<br />
+    		<button type="submit">Say Hello</button>
+    	</form>
     </body>    
 </html>
---
+++ sayhello.php
@@ -0,0 +1,8 @@
+<?php 
+
+print 'Hello, ';
+// Print what was submitted by the form
+print $_POST['user'];
+print "!";
+
+?>
